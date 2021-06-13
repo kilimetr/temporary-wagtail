663c023e13bc94dd9dc49582fd3f6c68dd44cca2
kilimetr <59143646+kilimetr@users.noreply.github.com>
2021-06-13 21:59:30 +0200

    Delete WLSLEVOVNIK directory

--- WLSLEVOVNIK/ccm_app/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class CcmAppConfig(AppConfig):
-    name = 'ccm_app'
--- WLSLEVOVNIK/polozka/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class PolozkaConfig(AppConfig):
-    name = 'polozka'
