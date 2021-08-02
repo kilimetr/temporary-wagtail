8fb471aebbd73de6fc9c5c6f63769ff1adcdb8f0
kilimetr <59143646+kilimetr@users.noreply.github.com>
2021-08-02 21:24:33 +0200

    Add files via upload

--- models.py
+++ models.py
@@ -1,38 +1,96 @@
+from datetime import datetime
 from django.db import models
 from longclaw.settings import PRODUCT_VARIANT_MODEL
+from longclaw.shipping.models import Address
 
-class BasketItem(models.Model):
-    basket_id = models.CharField(max_length=32)
-    date_added = models.DateTimeField(auto_now_add=True)
-    quantity = models.IntegerField(default=1)
-    variant = models.ForeignKey(PRODUCT_VARIANT_MODEL, unique=False, on_delete=models.PROTECT)
+class Order(models.Model):
+    SUBMITTED = 1
+    FULFILLED = 2
+    CANCELLED = 3
+    REFUNDED = 4
+    FAILURE = 5
+    ORDER_STATUSES = ((SUBMITTED, 'Submitted'),
+                      (FULFILLED, 'Fulfilled'),
+                      (CANCELLED, 'Cancelled'),
+                      (REFUNDED, 'Refunded'),
+                      (FAILURE, 'Payment Failed'))
+    payment_date = models.DateTimeField(blank=True, null=True)
+    created_date = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=ORDER_STATUSES, default=SUBMITTED)
+    status_note = models.CharField(max_length=128, blank=True, null=True)
 
-    class Meta:
-        ordering = ['date_added']
+    transaction_id = models.CharField(max_length=256, blank=True, null=True)
+
+    # contact info
+    email = models.EmailField(max_length=128, blank=True, null=True)
+    ip_address = models.GenericIPAddressField(blank=True, null=True)
+
+    # shipping info
+    shipping_address = models.ForeignKey(
+        Address, blank=True, null=True, related_name="orders_shipping_address", on_delete=models.PROTECT)
+
+    # billing info
+    billing_address = models.ForeignKey(
+        Address, blank=True, null=True, related_name="orders_billing_address", on_delete=models.PROTECT)
+
+    shipping_rate = models.DecimalField(max_digits=12,
+                                        decimal_places=2,
+                                        blank=True,
+                                        null=True)
 
     def __str__(self):
-        return "{}x {}".format(self.quantity, self.variant)
+        return "Order #{} - {}".format(self.id, self.email)
 
+    @property
     def total(self):
-        return self.quantity * self.variant.price
-
-    def name(self):
-        return self.variant.__str__()
-
-    def price(self):
-        return self.variant.price
-
-    def increase_quantity(self, quantity=1):
-        """ Increase the quantity of this product in the basket
+        """Total cost of the order
         """
-        self.quantity += quantity
+        total = 0
+        for item in self.items.all():
+            total += item.total
+        return total
+
+    @property
+    def total_items(self):
+        """The number of individual items on the order
+        """
+        return self.items.count()
+
+
+    def refund(self):
+        """Issue a full refund for this order
+        """
+        from longclaw.utils import GATEWAY
+        now = datetime.strftime(datetime.now(), "%b %d %Y %H:%M:%S")
+        if GATEWAY.issue_refund(self.transaction_id, self.total):
+            self.status = self.REFUNDED
+            self.status_note = "Refunded on {}".format(now)
+        else:
+            self.status_note = "Refund failed on {}".format(now)
         self.save()
 
-    def decrease_quantity(self, quantity=1):
+    def fulfill(self):
+        """Mark this order as being fulfilled
         """
+        self.status = self.FULFILLED
+        self.save()
+
+    def cancel(self, refund=True):
+        """Cancel this order, optionally refunding it
         """
-        self.quantity -= quantity
-        if self.quantity <= 0:
-            self.delete()
-        else:
-            self.save()
+        if refund:
+            self.refund()
+        self.status = self.CANCELLED
+        self.save()
+
+class OrderItem(models.Model):
+    product = models.ForeignKey(PRODUCT_VARIANT_MODEL, on_delete=models.DO_NOTHING)
+    quantity = models.IntegerField(default=1)
+    order = models.ForeignKey(Order, related_name='items', on_delete=models.CASCADE)
+
+    @property
+    def total(self):
+        return self.quantity * self.product.price
+
+    def __str__(self):
+        return "{} x {}".format(self.quantity, self.product.get_product_title())
